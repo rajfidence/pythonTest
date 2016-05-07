8c7edb7a1d536e9ec9b86e0a1cc8750b2ca70245
Raj Parekh <rockingraj2026@gmail.com>
2016-05-07 17:27:00 +0530

    commit from pycharm

---
+++ website.py
@@ -0,0 +1,21 @@
+__author__ = 'raj'
+import socket
+
+HOST = ''
+PORT = 8080
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+connection, address = listen_socket.accept()
+request = connection.recv(1024)
+connection.sendall("""HTTP/1.1 200 OK
+Content-type: text/html
+
+
+<html>
+    <body>
+        <h1>Hello, World!</h1>
+    </body>
+</html>""")
+
+connection.close()
